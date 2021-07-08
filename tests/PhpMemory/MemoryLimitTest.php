fc3a9b96a8230245229fb740b99a9b0b018205f3
Sam Litowitz <samlitowitz@users.noreply.github.com>
2021-07-07 20:31:47 -0400

    Add tests

--- tests/PhpMemory/MemoryLimitTest.php
+++ tests/PhpMemory/MemoryLimitTest.php
@@ -2,8 +2,11 @@
 
 namespace PhpMemory;
 
+use PhpMemory\Unit\Binary\Gibibyte;
 use PhpMemory\Unit\Binary\Gigabyte;
+use PhpMemory\Unit\Binary\Kibibyte;
 use PhpMemory\Unit\Binary\Kilobyte;
+use PhpMemory\Unit\Binary\Mebibyte;
 use PhpMemory\Unit\Binary\Megabyte;
 use PhpMemory\Unit\Byte;
 use PHPUnit\Framework\TestCase;
@@ -81,4 +84,99 @@ final class MemoryLimitTest extends TestCase
 		$this->expectException(InvalidArgumentException::class);
 		MemoryLimit::get();
 	}
+
+	/**
+	 * @dataProvider set_as_bytes_provider
+	 */
+	public function test_set_as_bytes(Size $expected)
+	{
+		MemoryLimit::set($expected);
+		$actual = MemoryLimit::get();
+
+		$this->assertNotNull($actual);
+		$this->assertEquals($expected->getBytes(), $actual->getBytes());
+	}
+
+	public function set_as_bytes_provider()
+	{
+		return [
+			'100 bytes' => [
+				Size::create(100, new Byte()),
+			],
+			'1 kibibyte' => [
+				Size::create(1, new Kibibyte()),
+			],
+			'1 kilobyte' => [
+				Size::create(1, new Kilobyte()),
+			],
+			'1 mebibyte' => [
+				Size::create(1, new Mebibyte()),
+			],
+			'1 megabyte' => [
+				Size::create(1, new Megabyte()),
+			],
+			'1 gibibyte' => [
+				Size::create(1, new Gibibyte()),
+			],
+			'1 gigabyte' => [
+				Size::create(1, new Gigabyte()),
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider set_as_string_provider
+	 */
+	public function test_set_as_string(Size $size, string $expected)
+	{
+		MemoryLimit::set($size, true);
+		$actual = ini_get(MemoryLimit::INI_OPTION);
+
+		$this->assertNotNull($actual);
+		$this->assertNotFalse($actual);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function set_as_string_provider()
+	{
+		return [
+			'100 bytes' => [
+				Size::create(100, new Byte()),
+				'100',
+			],
+			'1 kibibyte' => [
+				Size::create(1, new Kibibyte()),
+				'1K',
+			],
+			'1 kilobyte' => [
+				Size::create(1, new Kilobyte()),
+				'1K',
+			],
+			'1 mebibyte' => [
+				Size::create(1, new Mebibyte()),
+				'1M',
+			],
+			'1 megabyte' => [
+				Size::create(1, new Megabyte()),
+				'1M',
+			],
+			'1 gibibyte' => [
+				Size::create(1, new Gibibyte()),
+				'1G',
+			],
+			'1 gigabyte' => [
+				Size::create(1, new Gigabyte()),
+				'1G',
+			],
+		];
+	}
+
+	public function test_set_no_limit()
+	{
+		MemoryLimit::set(null);
+		$actual = MemoryLimit::get();
+
+		$this->assertNull($actual);
+	}
+
 }
